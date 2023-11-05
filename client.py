1377473792b73f4d392066ee4d8c648c3ae3e071
Teodor Angelovski <angelovski.teo@outlook.com>
2023-11-05 23:20:33 +0100

    Initial commit

---
+++ client.py
@@ -0,0 +1,69 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
+QML = """
+import QtQuick
+import QtQuick.Controls
+import QtQuick.Layouts
+
+Window {{
+    height: 600
+    width: 800
+    visible: true
+    title: "E-Shop"
+
+    readonly property list<string> titles: {}
+    readonly property list<string> prices: {}
+
+    function changeProduct() {{
+        var i = Math.round(Math.random() * 3)
+        title.text = titles[i]
+        price.text = prices[i]
+    }}
+
+    ColumnLayout {{
+        anchors.fill:  parent
+
+        Text {{
+            id: title
+            text: "Product title"
+            Layout.alignment: Qt.AlignHCenter
+        }}
+        Text {{
+            id: price
+            text: "Product price"
+            Layout.alignment: Qt.AlignHCenter
+        }}
+        Button {{
+            text: "Next product"
+            Layout.alignment: Qt.AlignHCenter
+            onClicked:  changeProduct()
+        }}
+    }}
+}}
+"""
+
+if __name__=="__main__":
+    eshop_api = "https://shagged-destroyers.000webhostapp.com/public/index.php/api"
+    response = requests.get(eshop_api)
+    content = BeautifulSoup(response.text)
+
+    titles = []
+    prices = []
+    for product in response.json():
+        titles.append(product["name"])
+        prices.append(product["price"])
+    QML = QML.format(titles, prices)
+
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.loadData(QML.encode('utf-8'))
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+    exit_code = app.exec()
+    del engine
+    sys.exit(exit_code)
